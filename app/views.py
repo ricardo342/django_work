ce25fef67ed87d5e947152ddbc6422f8292ff822
majiaqin <majiaqin@techb.top>
2022-10-10 18:47:35 +0800

    修改文件信息

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+'''建立一个http简单响应'''
+def index(request):
+    return HttpResponse('Hello World')
